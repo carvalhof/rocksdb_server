a8dce2c3f9b3af374d05e6fc40f66b0d2f8158a2
carvalhof <carvalhof.br@gmail.com>
2024-06-21 16:20:21 -0400

    wip

---
+++ Makefile
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+TARGET = server
+
+CXX = g++
+CXXFLAGS = -Wall -Wextra -Werror -pedantic -pedantic-errors \
+	-Wunused -Wuninitialized -Wfloat-equal -Wshadow \
+	-O2 -fstack-protector-strong -fsanitize=address \
+	-pthread -std=c++17 -I/home/fbc/rocksdb/include
+LDFLAGS = -L/home/fbc/rocksdb -lrocksdb -lpthread -lsnappy -lz -lbz2 -llz4 -lzstd -ldl -fsanitize=address
+
+SRCS = main.cpp
+
+OBJS = $(SRCS:.cpp=.o)
+
+all: $(TARGET)
+
+$(TARGET): $(OBJS)
+	$(CXX) -o $@ $^ $(LDFLAGS)
+
+%.o: %.cpp
+	$(CXX) $(CXXFLAGS) -c $< -o $@
+
+clean:
+	rm -f $(OBJS) $(TARGET)
---
+++ main.cpp
@@ -0,0 +1,113 @@
+#include <iostream>
+#include <string>
+#include <map>
+#include <rocksdb/db.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <sstream>
+#include <thread>
+
+#define BUFFER_SIZE 1024
+
+void handle_client(int client_socket, rocksdb::DB* db) {
+    char buffer[BUFFER_SIZE] = {0};
+    while (true) {
+        int valread = read(client_socket, buffer, BUFFER_SIZE);
+        if (valread <= 0) break;
+
+        std::string command(buffer, valread);
+        std::istringstream iss(command);
+        std::string operation;
+        iss >> operation;
+
+        if (operation == "SET") {
+            std::string key, value;
+            iss >> key >> value;
+            rocksdb::Status status = db->Put(rocksdb::WriteOptions(), key, value);
+            std::string response = status.ok() ? "OK\n" : "FAIL\n";
+            send(client_socket, response.c_str(), response.size(), 0);
+        } else if (operation == "GET") {
+            std::string key, value;
+            iss >> key;
+            rocksdb::Status status = db->Get(rocksdb::ReadOptions(), key, &value);
+            std::string response = status.ok() ? value + "\n" : "NOT_FOUND\n";
+            send(client_socket, response.c_str(), response.size(), 0);
+        } else if (operation == "SCAN") {
+            std::string start_key, end_key;
+            iss >> start_key >> end_key;
+            std::string response;
+            auto it = db->NewIterator(rocksdb::ReadOptions());
+            for (it->Seek(start_key); it->Valid() && it->key().ToString() <= end_key; it->Next()) {
+                response += it->key().ToString() + " : " + it->value().ToString() + "\n";
+            }
+            delete it;
+            send(client_socket, response.c_str(), response.size(), 0);
+        }
+
+        //std::cout << "Command received: " << command << std::endl;
+    }
+    close(client_socket);
+}
+
+int main(int argc, char* argv[]) {
+    if (argc < 3) {
+        std::cerr << "Usage: " << argv[0] << " <port> <path_to_db>" << std::endl;
+        return 1;
+    }
+
+    int port = std::stoi(argv[1]);
+    std::string db_path = argv[2];
+    rocksdb::DB* db;
+    rocksdb::Options options;
+    options.create_if_missing = true;
+    rocksdb::Status status = rocksdb::DB::Open(options, db_path, &db);
+
+    if (!status.ok()) {
+        std::cerr << "Unable to open/create database: " << status.ToString() << std::endl;
+        return 1;
+    }
+
+    int server_fd, new_socket;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        std::cerr << "Socket failed" << std::endl;
+        return 1;
+    }
+
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+        std::cerr << "Setsockopt failed" << std::endl;
+        return 1;
+    }
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(port);
+
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        std::cerr << "Bind failed" << std::endl;
+        return 1;
+    }
+
+    if (listen(server_fd, 3) < 0) {
+        std::cerr << "Listen failed" << std::endl;
+        return 1;
+    }
+
+    std::cout << "Server started on port " << port << std::endl;
+
+    while (true) {
+        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
+            std::cerr << "Accept failed" << std::endl;
+            return 1;
+        }
+
+        std::cout << "New connection established" << std::endl;
+        std::thread(handle_client, new_socket, db).detach();
+    }
+
+    delete db;
+    return 0;
+}
